e0b12a537274b78c62e39666db2e37fabe4f2cdb
d2dspot <d2dspotz@gmail.com>
2025-08-05 18:01:56 +0530

    Update ScrollToTop.jsx

--- src/lib/ScrollToTop.jsx
+++ src/lib/ScrollToTop.jsx
@@ -5,12 +5,16 @@ const ScrollToTop = () => {
     const { pathname, hash } = useLocation();
 
     useEffect(() => {
-        if (!hash) {
-            window.scrollTo({ top: 0, behavior: 'instant' });
+        if (!hash) { 
+            window.scrollTo({
+                top: 0,
+                behavior: "instant",
+            });
         } else {
-            const element = document.getElementById(hash.substring(1));
+            const element = document.getElementById(hash.substring(1)); // Remove the '#' from hash
             if (element) {
-                element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                const top=element.getBoundingClientRect().top;
+                element.scrollIntoView({ behavior: 'smooth', block: 'start',bottom:top });
             }
         }
     }, [pathname, hash]);
